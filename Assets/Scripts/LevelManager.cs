20882408ef8d82ae5fe1c7665fcb467a40d31cfa
gamingod4u <mcman407@gmail.com>
2014-08-30 14:39:40 -0400

    Update

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour 
+{
+	public int levelNum = 0;
+	public int ballsCount = 0;
+	public int playerLives = 0;
+
+
+	private int ballsAlive = 0;
+	private Factory	gameFactory;
+	// Use this for initialization
+	void Start () 
+	{
+		gameFactory = GameObject.Find("Factory").GetComponent<Factory>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		while(ballsAlive < ballsCount)
+		{
+
+		}
+	}
+}
