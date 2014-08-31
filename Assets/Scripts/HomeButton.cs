dc90bd2fb2c4b80dc75cb41238178c410206d42d
John Li <johnliglobal@gmail.com>
2014-08-31 17:37:12 -0400

    Added home button to credits

---
+++ Assets/Scripts/HomeButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomeButton : MonoBehaviour {
+
+	
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			if(guiTexture.GetScreenRect().Contains(Input.mousePosition))
+				Application.LoadLevel(0); //main menu
+		}
+	}
+}
