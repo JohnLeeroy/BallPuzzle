50dc0febcb95a144f8b64d78871753578389a38a
Jill <jg24drive45@gmail.com>
2014-08-31 11:53:47 -0400

    file save and load data, highscores scene

---
+++ Assets/Scripts/Highscores.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Highscores : MonoBehaviour
+{
+	void Start()
+	{
+		IList data = File.GetHighscores();
+		
+		string names = "";
+		string scores = "";
+		int i = 0;
+		
+		foreach(IDictionary scoreData in data)
+		{
+			if( i < 9 )
+			{
+				names += scoreData["Initials"] + "\n";
+				scores += scoreData["Score"] + "\n";
+			}
+			else
+			{
+				names += scoreData["Initials"];
+				scores += scoreData["Score"];
+			}
+			i++;
+		}
+
+		GameObject.Find( "Names" ).GetComponent<GUIText>().guiText.text = names;
+		GameObject.Find( "Scores" ).GetComponent<GUIText>().guiText.text = scores;
+	}
+	
+	void Update()
+	{
+		if( Input.GetKeyDown( KeyCode.Return ) )
+			File.SaveScore( "JJJ", 54321 );
+	}
+}
