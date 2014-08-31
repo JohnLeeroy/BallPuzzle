02454d9ade10c0a55901efc1b81bdaf00eecd91b
John Li <johnliglobal@gmail.com>
2014-08-31 17:16:26 -0400

    Handled Main Menu

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuUI : MonoBehaviour {
+
+	public GUITexture startButton;
+	public GUITexture creditsButton;
+	public GUITexture leaderboardButton;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector2 mousePos = Input.mousePosition;
+			if( startButton.GetScreenRect().Contains(mousePos))
+			{
+				Debug.Log("Start Button");
+				Application.LoadLevel("Arcade");
+			}
+			else if(creditsButton.GetScreenRect().Contains(mousePos))
+			{
+				Debug.Log("Credits Button");
+				
+			}
+			else if(leaderboardButton.GetScreenRect().Contains(mousePos))
+			{
+				Debug.Log("Leaderboard Button");
+				
+			}
+		}
+	}
+}
