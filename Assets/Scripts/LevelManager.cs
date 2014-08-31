34a93918fa4bfcf4052dc1aba2615190e2145ce8
gamingod4u <mcman407@gmail.com>
2014-08-31 14:12:23 -0400

    Particles are in and uniform with background

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,6 +7,7 @@ public class LevelManager : MonoBehaviour
 	public static LevelManager Instance;
 	public int levelNum    = 0;
 	public int ballsCount  = 5;
+	public int randomizedNum = 0;
 	public List<Material>  	backgroundMaterials;
 	public GameObject		background;
 	public List<Material>   playbarMaterials;
@@ -29,9 +30,7 @@ public class LevelManager : MonoBehaviour
 	{
 		Instance = this;
 		bubbles = new List<Transform> (ballsCount);
-		int tempInt = Random.Range(0,4);
-		background.renderer.material = backgroundMaterials[tempInt];
-		playbar.renderer.material = playbarMaterials[tempInt];
+	
 	}
 
 	void Start () 
@@ -47,7 +46,9 @@ public class LevelManager : MonoBehaviour
 			bubbles.Add(newBubble.transform);
 			//TODO Randomly position bubbles
 		}
-
+		randomizedNum = Random.Range(0,4);
+		background.renderer.material = backgroundMaterials[randomizedNum];
+		playbar.renderer.material = playbarMaterials[randomizedNum];
 	}
 
 }
