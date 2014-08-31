73846a60478576ea1dad4942a6da5a4ff2c4dcea
John Li <johnliglobal@gmail.com>
2014-08-31 14:36:33 -0400

    Fixed bug

--- Assets/Scripts/ParticleManager.cs
+++ Assets/Scripts/ParticleManager.cs
@@ -8,7 +8,7 @@ public class ParticleManager : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		levelMang = LevelManager.Instance;
+		levelMang = GameObject.Find("LevelManager").GetComponent<LevelManager>();
 		NotificationCenter.DefaultCenter.AddObserver (this, "OnBubblePop");
 	}
 	void OnBubblePop(NotificationCenter.Notification notify)
